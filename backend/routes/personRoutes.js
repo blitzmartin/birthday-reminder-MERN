f80fed1f7e2fb6a26678b87e0c74b9ba717158f8
blitzmartin <lizmartinmusic@gmail.com>
2022-07-10 19:40:51 +0200

    app with backend working, getting data from db

--- backend/routes/personRoutes.js
+++ backend/routes/personRoutes.js
@@ -5,10 +5,20 @@ import Person from '../models/personModel.js';
 const personRouter = express.Router();
 
 
-personRouter.get('/', async (req, res) => {
+/* personRouter.get('/', async (req, res) => {
     const people = await Person.find();
     res.send(people);
 });
+ */
+personRouter.get('/', async (req, res) => {
+    const people = await Person.find({})
+    .then(data => {
+      res.status(200).json(data)
+    })
+    .catch((err) => console.error(err.message))
+
+});
+
 
 personRouter.post('/addperson', expressAsyncHandler(async( req, res) => {
     const newPerson = await Person.create({
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import data from "../data.js";
+//import data from "../data.js";
+import axios from "axios";
 import Container from "react-bootstrap/Container";
 import List from "../components/List";
 
@@ -9,23 +10,34 @@ const mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
 const day = dd + mm;
 
 export default function Home() {
-  const [people, setPeople] = useState(data);
-  const [ counter, setCounter ] = useState(0)
+  const [people, setPeople] = useState([]);
+  const [counter, setCounter] = useState(0);
+
+  function getPeople() {
+    fetch("/api/contacts")
+      .then((res) => res.json())
+      .then((data) => {
+        setPeople(data);
+      })
+      .catch((err) => console.log(err));
+    return;
+  }
 
   const countBdays = () => {
     let addOne = 0;
-    for (let i = 0; i < data.length; i++){
-        if (data[i].bday=== day) {
-           addOne++;
-        }
+    for (let i = 0; i < people.length; i++) {
+      if (people[i].bday === day) {
+        addOne++;
+      }
     }
-    setCounter(addOne)
+    setCounter(addOne);
     return counter;
-  }
+  };
 
   useEffect(() => {
+    getPeople();
     countBdays();
-  }, [counter]);
+  }, []);
 
   return (
     <div>
