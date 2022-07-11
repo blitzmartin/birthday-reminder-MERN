f9da32f34dd833eb6d1d9c2722615314a7ee8dcd
blitzmartin <lizmartinmusic@gmail.com>
2022-07-11 19:24:11 +0200

    setup for heroku

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import personRouter from './routes/personRoutes.js';
@@ -43,6 +44,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
 app.use('/api/contacts', personRouter);
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
 
 app.use((err, req, res, next) => {
     res.status(500).send({ message: err.message })
